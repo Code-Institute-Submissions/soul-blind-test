1480262fc83eaf4f9c494c23f23778968ce6a2c0
Mathilde <mathilde206@gmail.com>
2018-02-20 15:07:39 +0000

    Add the countdown on the game page

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,17 @@
 setTimeout(function(){
   document.getElementById('submitGuess').click();
 }, 33000);
+
+
+let x = setInterval(function() {
+  let currentSecs = $("#seconds").text();
+  console.log(currentSecs);
+  let newSecs = Number(currentSecs) - 1; 
+  console.log(newSecs);
+  $("#seconds").text(newSecs.toString());
+  
+}, 1000)
+
+if(Number($("#seconds").text()) === 0){
+  clearInterval(x);
+}
